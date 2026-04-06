533233036930c41f3ce1aa109333ad5c7cb818ce
김소희 <sohee2655@naver.com>
2026-04-06 13:06:03 +0900

    9095

--- baekjoon/9095/solution.py
+++ baekjoon/9095/solution.py
@@ -1,18 +1,49 @@
 import sys
 input = sys.stdin.readline
 
-T = int(input())
+#갯수 입력
+N = int(input())
 
-# 최대 n이 10이니까 미리 다 구해놓기
+#Top-dowm
+memo = {}
+
+def dfs(K) :
+  #base case
+  if K == 1 : return 1
+  if K == 2 : return 2
+  if K == 3 : return 4
+
+  #memo out
+  if K in memo:
+    return memo[K]
+
+  #memoization
+  if K not in memo:
+    memo[K] = dfs(K-1) + dfs(K-2) + dfs(K-3)
+    return memo[K]
+  
+for _ in range(N):
+  K = int(input())
+  print(dfs(K))
+
+
+#bottom-up
+
+#갯수 입력
+N = int(input())
+
+#배열
 dp = [0] * 11
 
-dp[1] = 1  # 1
-dp[2] = 2  # 1+1, 2
-dp[3] = 4  # 1+1+1, 1+2, 2+1, 3
+#초기식
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
 
-for i in range(4, 11):
-    dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+#점화식 - 미리 다 계산
+for i in range(4, 11) :
+  dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
 
-for _ in range(T):
-    n = int(input())
-    print(dp[n])
+for _ in range(N) :
+  k = int(input())
+  print(dp[k])
