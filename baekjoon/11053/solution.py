6deea50784fc29c2534da556e8e8e01755c0e666
김소희 <sohee2655@naver.com>
2026-04-07 14:57:02 +0900

    11053

---
+++ baekjoon/11053/solution.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+lst = list(map(int, input().split()))
+
+# lst 수열과 같은 수열을 만들어
+lenlst = [1] * len(lst)
+
+#i번째랑 이 전 값을 비교해서 크면 lenlst에 큰 값을 저장, 작으면 ㅃㅇ
+for i in range(len(lst)):
+  j = 0
+  for j in range(i):
+    
+    if lst[j] < lst[i] :
+      lenlst[i] = max( lenlst[i], lenlst[j]+1 )
+
+print(max(lenlst))
