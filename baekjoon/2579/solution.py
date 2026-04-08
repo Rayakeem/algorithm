442a954176a43b666f75a4facd3de8e3010f57a4
김소희 <sohee2655@naver.com>
2026-04-08 16:15:00 +0900

    top-down

--- baekjoon/2579/solution.py
+++ baekjoon/2579/solution.py
@@ -1,30 +1,56 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input())
+# N = int(input())
 
-# [0] + cost_list를 받아서 층 수를 맞춰준다.
-# 1층과 2층은 베이스케이스로 구해놓는다
-# 3층부터 N층까지 반복문을 돌면서 구한다.
-# N층을 출력.
+# # [0] + cost_list를 받아서 층 수를 맞춰준다.
+# # 1층과 2층은 베이스케이스로 구해놓는다
+# # 3층부터 N층까지 반복문을 돌면서 구한다.
+# # N층을 출력.
 
-# 중요: 시작점을 제외한 계단을 연속으로 한 칸씩 총 두칸을 연속으로 오를 수 없으니, 점화식은 max( (N-2) , (N-3) + cost[N-1] ) + cost[N]
+# # 중요: 시작점을 제외한 계단을 연속으로 한 칸씩 총 두칸을 연속으로 오를 수 없으니, 점화식은 max( (N-2) , (N-3) + cost[N-1] ) + cost[N]
 
-# bottom-up
+# # bottom-up
 
-cost = [0] + [ int(input()) for _ in range(N) ]
-# 이렇게 하면 [0, 1,2,3,4,5] 인덱스가 실제 층수와 동일 (0층을 없애기 위함)
+# cost = [0] + [ int(input()) for _ in range(N) ]
+# # 이렇게 하면 [0, 1,2,3,4,5] 인덱스가 실제 층수와 동일 (0층을 없애기 위함)
 
-memo = [0] * (N+1)
+# memo = [0] * (N+1)
 
-if N < 2 : print(cost[N])
-else :
-  memo[1] = cost[1]
-  memo[2] = cost[2] + memo[1]
+# if N < 2 : print(cost[N])
+# else :
+#   memo[1] = cost[1]
+#   memo[2] = cost[2] + memo[1]
 
-  for i in range(3, N+1) :
-    memo[i] =max(
-        memo[i-2], memo[i-3] + cost[i-1]
-      ) + cost[i]
+#   for i in range(3, N+1) :
+#     memo[i] =max(
+#         memo[i-2], memo[i-3] + cost[i-1]
+#       ) + cost[i]
   
-  print(memo[N])
+#   print(memo[N])
+
+#Top-down
+
+N = int(input())
+cost = [0] + [ int(input()) for _ in range(N)]
+
+memo = [-1] * (N+1)
+
+def dp(i) : 
+  #base case
+  if i == 0 : return 0
+  if i == 1 : return cost[1]
+  if i == 2 : return cost[1] + cost[2] 
+
+  #memo
+  if memo[i] != -1 : return memo[i]
+
+  #memoization
+  memo[i] = max(
+      dp(i-2) , dp(i-3) + cost[i-1]
+    ) + cost[i]
+
+  return memo[i]
+
+
+print(dp(N))
