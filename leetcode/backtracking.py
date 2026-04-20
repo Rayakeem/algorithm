39bf6401157a052dba8f69006cdaa580c8410113
김소희 <sohee2655@naver.com>
2026-04-20 15:14:12 +0900

    backtracking

---
+++ leetcode/backtracking.py
@@ -0,0 +1,20 @@
+#조합 
+
+def solution(nums, k) :
+  result = []
+
+  def backtracking(start, curr):
+    #basecase
+    if len(curr) == k :
+      result.append(curr[:])
+      return
+
+    for i in range(start, len(nums)):
+      curr.append(nums[i])
+      backtracking(i+1, curr)
+      curr.pop()
+
+  backtracking(start = 0, curr = [])
+  return result
+
+print(solution(nums=[1,2,3,4], k = 2))
